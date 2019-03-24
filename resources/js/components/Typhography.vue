41236a13802f99ef3d046f9c137559ca68d6710b
uzzwal <d.uzzwal09@gmail.com>
2019-03-24 22:33:48 +0600

    Eight vues added

---
+++ resources/js/components/Typhography.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="font-normal text-3xl text-grey-darkest leading-none">Typhygraphy</h1>
+</template>
+
+<script>
+    export default {};
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -21,10 +21,10 @@
                         <h5 class="uppercase font-bold mb-3">The Brand</h5>
 
                         <ul class="list-reset">
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo Symbol</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Colors</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Typhography</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/" exact>Logo</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/logo-symbol">Logo Symbol</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/colors">Colors</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/typhography">Typhography</router-link></li>
                         </ul>
                     </section>
 
@@ -32,10 +32,10 @@
                         <h5 class="uppercase font-bold mb-3">Doodles</h5>
 
                         <ul class="list-reset">
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Mascot</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Illustrations</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Loaders and Animations</router-link></li>
-                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Wallpapers</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/mascot">Mascot</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/illustrations">Illustrations</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/loaders-and-animations">Loaders and Animations</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/wallpapers">Wallpapers</router-link></li>
                         </ul>
                     </section>
                 </aside>
