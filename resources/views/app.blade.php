f2764fd9798dd909d1bf3a8f5f4a65d207d5f966
uzzwal <d.uzzwal09@gmail.com>
2019-03-24 21:05:16 +0600

    Just uploaded with few links!

---
+++ resources/views/app.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+
+    </head>
+    <body class="font-sans">
+        <div class="container mx-auto" id="app">
+            <header class="py-6 mb-8">
+                <h1>Single Page Application</h1>
+            </header>
+            
+            <main class="flex">
+                <aside class="w-1/5">
+                    <section class="mb-8">
+                        <h5 class="uppercase font-bold mb-3">The Brand</h5>
+
+                        <ul class="list-reset">
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Logo Symbol</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Colors</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Typhography</router-link></li>
+                        </ul>
+                    </section>
+
+                    <section class="mb-8">
+                        <h5 class="uppercase font-bold mb-3">Doodles</h5>
+
+                        <ul class="list-reset">
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/">Mascot</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Illustrations</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Loaders and Animations</router-link></li>
+                            <li class="text-sm leading-loose"><router-link class="text-black" to="/about">Wallpapers</router-link></li>
+                        </ul>
+                    </section>
+                </aside>
+
+                <div class="primary flex-1">
+                    <router-view></router-view>
+                </div>
+            </main>
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
